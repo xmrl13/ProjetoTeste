1fdbb3beff128e4c2cd548a6105b67a2f0820f29
Marcelo de Oliveira <109050359+xmrl13@users.noreply.github.com>
2022-07-11 21:04:38 -0300

    comitei testando

--- teste.py
+++ teste.py
@@ -2,8 +2,3 @@ print('Hellow World')
 a=int(input('Digite um valor:'))
 b=int(input('Digite outro valor: '))
 print (f'{a+b}')
-print('Testando integração entre Pycharm e VSCode por Git')
-if a+b>35:
-    print('A soma de A e B é maior que 35')
-else:
-    print('A soma de A e B não é maior que 35')
